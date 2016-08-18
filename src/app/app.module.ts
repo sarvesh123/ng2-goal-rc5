8f7a691d7af3c6cf0b23bdc4bba50b266e6858e1
Sarvesh Shejwadkar <sarvesh@sjinnovation.com>
2016-08-18 14:19:35 +0530

    add signup form http service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule, JsonpModule } from '@angular/http';
 
 import { AppComponent }   from './app.component';
 import { SignupComponent } from './components/signup/signup.component';
@@ -11,7 +13,10 @@ import { routing,
 @NgModule({
     imports:      [
         BrowserModule,
-        routing
+        routing,
+        FormsModule,
+        HttpModule,
+        JsonpModule
     ],
     declarations: [
         AppComponent,
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
+
 @Component({
   moduleId: module.id,
   selector: 'app-signup',
@@ -8,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  user = new User(1, 'sarvesh shejwadkar', 'a@a.com', '123456');
+
+  errorMessage: string;
+
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+  }
+
+  newUser() {
+    this.user = new User(1,'','','');
+  }
+
+  onSubmit(name: string, email: string, password: string) {
+    this.userService.addUser(name, email, password)
+                    .subscribe(
+                      user => user,
+                      error => this.errorMessage = <any>error);
   }
 
 }
