cd2f0c1f9370c5b29e0bb6d59e823a067e255d2d
Sarvesh Shejwadkar <sarveshshejwadkar@gmail.com>
2016-09-09 22:10:49 +0530

    update twitter component spec

--- src/app/components/login/twitter/twitter.component.spec.ts
+++ src/app/components/login/twitter/twitter.component.spec.ts
@@ -4,10 +4,11 @@ import { By }           from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 import { addProviders, async, inject } from '@angular/core/testing';
 import { TwitterComponent } from './twitter.component';
+import { UserService } from '../../../shared/user.service';
 
 describe('Component: Twitter', () => {
-  it('should create an instance', () => {
-    let component = new TwitterComponent();
+  it('should create an instance', inject([UserService], (userService: UserService) => {
+    let component = new TwitterComponent(userService);
     expect(component).toBeTruthy();
-  });
+  }));
 });
